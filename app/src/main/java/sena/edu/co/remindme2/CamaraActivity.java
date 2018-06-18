64f455b600553a3499a8362ced7267afec91df04
Andres Orozco <daorozco665@misena.edu.co>
2018-06-18 15:13:42 -0500

    chore: Implementacion de Floating Button

--- app/src/main/java/sena/edu/co/remindme2/CamaraActivity.java
+++ app/src/main/java/sena/edu/co/remindme2/CamaraActivity.java
@@ -6,9 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class CamaraActivity extends AppCompatActivity {
+public class CamaraActivity extends AppCompatActivity implements View.OnClickListener{
 
+    FloatingActionButton fab_plus;
+    FloatingActionButton fab_galery;
+    FloatingActionButton fab_share;
+
+    Animation anim_open;
+    Animation anim_close;
+    Animation anim_forward;
+    Animation anim_backward;
+
+    Boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +29,74 @@ public class CamaraActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab_plus = (FloatingActionButton) findViewById(R.id.fab_plus);
+        fab_galery = (FloatingActionButton) findViewById(R.id.fab_galery);
+        fab_share = (FloatingActionButton) findViewById(R.id.fab_share);
+
+        anim_open = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        anim_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        anim_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_forward);
+        anim_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_backward);
+
+        fab_plus.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.fab_plus:
+
+                if(isOpen){
+                    fab_plus.startAnimation(anim_backward);
+                    fab_galery.startAnimation(anim_close);
+                    fab_share.startAnimation(anim_close);
+                    fab_galery.setClickable(false);
+                    fab_galery.setClickable(false);
+                    isOpen = false;
+                } else {
+                    fab_plus.startAnimation(anim_forward);
+                    fab_galery.startAnimation(anim_open);
+                    fab_share.startAnimation(anim_open);
+                    fab_galery.setClickable(true);
+                    fab_galery.setClickable(true);
+                    isOpen = true;
+                }
+
+                break;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
