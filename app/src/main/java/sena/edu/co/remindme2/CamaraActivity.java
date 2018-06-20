8f3b69e4ef57a777a0deb3363a9ac1ed40dcefb0
Andres Orozco <daorozco665@misena.edu.co>
2018-06-20 15:35:18 -0500

    chore: Implementacion de Branch "TestBranch"

--- app/src/main/java/sena/edu/co/remindme2/CamaraActivity.java
+++ app/src/main/java/sena/edu/co/remindme2/CamaraActivity.java
@@ -97,7 +97,6 @@ public class CamaraActivity extends AppCompatActivity implements View.OnClickLis
         Intent intentCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intentCamara.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
         startActivityForResult(intentCamara, COD_IMAGEN);
-
     }
 
     @Override
