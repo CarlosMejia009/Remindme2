42c8b38037dc317927f9b296dd1c51e00780c579
Andres Orozco <daorozco665@misena.edu.co>
2018-06-20 15:22:22 -0500

    chore: Se implemento la toma y visualizacion de Imagenes

--- app/src/main/java/sena/edu/co/remindme2/CamaraActivity.java
+++ app/src/main/java/sena/edu/co/remindme2/CamaraActivity.java
@@ -2,6 +2,9 @@ package sena.edu.co.remindme2;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -9,10 +12,13 @@ import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 import java.io.File;
 
 public class CamaraActivity extends AppCompatActivity implements View.OnClickListener{
@@ -28,8 +34,9 @@ public class CamaraActivity extends AppCompatActivity implements View.OnClickLis
     Animation anim_backward;
     Boolean isOpen = false;
 
+    ImageView imgPrueba;
 
-    private final String CARPETA_RAIZ="Galeria/";
+    private final String CARPETA_RAIZ="GaleryWS/";
     private final String RUTA_IMAGEN=CARPETA_RAIZ+"misFotos";
     private final int COD_GALERY = 10;
     private final int COD_IMAGEN = 20;
@@ -49,6 +56,7 @@ public class CamaraActivity extends AppCompatActivity implements View.OnClickLis
         fab_camara = (FloatingActionButton) findViewById(R.id.fab_camara);
         fab_galery = (FloatingActionButton) findViewById(R.id.fab_galery);
         fab_share = (FloatingActionButton) findViewById(R.id.fab_share);
+        imgPrueba = (ImageView) findViewById(R.id.imgPrueba);
 
         anim_open = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
         anim_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
@@ -72,18 +80,52 @@ public class CamaraActivity extends AppCompatActivity implements View.OnClickLis
         boolean isCreated = fileImagen.exists();
         String nombreImg = "";
 
-        if (isCreated) {
-            nombreImg = (System.currentTimeMillis() / 1000) + ".jpg";
-        } else {
+        if (!isCreated){
             isCreated = fileImagen.mkdirs();
         }
+
+        if (isCreated) {
+            nombreImg = (System.currentTimeMillis() / 1000) + ".jpg";
+        }
+
+
         path = Environment.getExternalStorageDirectory() +
                 File.separator + RUTA_IMAGEN + File.separator + nombreImg;
+
         File imagen = new File(path);
 
         Intent intentCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intentCamara.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
         startActivityForResult(intentCamara, COD_IMAGEN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            switch (requestCode)
+            {
+                case COD_GALERY:
+                    Uri miPath = data.getData();
+                    imgPrueba.setImageURI(miPath);
+                    break;
+
+                case COD_IMAGEN:
+                    MediaScannerConnection.scanFile(this,new String[]{path}, null,
+                            new MediaScannerConnection.OnScanCompletedListener(){
+
+                                @Override
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("Ruta de la Imagen ","Path: "+path);
+                                }
+                            });
+                    Bitmap bitmap = BitmapFactory.decodeFile(path);
+                    imgPrueba.setImageBitmap(bitmap);
+                    break;
+            }
+
+
+        }
     }
 
     @SuppressLint("RestrictedApi")
@@ -144,6 +186,7 @@ public class CamaraActivity extends AppCompatActivity implements View.OnClickLis
                 break;
         }
     }
+
 }
 
 
