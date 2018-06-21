e7457764c042ed447e62e4824f4d403b43e53a49
Carlos Mejia <camejia009@misena.edu.co>
2018-06-21 17:33:04 -0500

    chore: Se sube diseño de la clase Photo.

--- app/src/main/java/sena/edu/co/remindme2/PhotoActivity.java
+++ app/src/main/java/sena/edu/co/remindme2/PhotoActivity.java
@@ -1,6 +1,9 @@
 package sena.edu.co.remindme2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +11,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PhotoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView imagen;
 
     FloatingActionButton fab_plus_photo;
     FloatingActionButton fab_audio_photo;
@@ -28,6 +35,8 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_photo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imagen = findViewById(R.id.imgPhoto);
 
         anim_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
         anim_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
@@ -70,6 +79,29 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
                     isOpen = true;
                 }
                 break;
+            case R.id.btnSeleccionar:
+
+                cargarImagen();
+
+                break;
+
+        }
+    }
+
+    private void cargarImagen() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la aplicación"),10);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Uri path = data.getData();
+            imagen.setImageURI(path);
         }
     }
 }
